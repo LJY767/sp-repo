eadd1557bf15770fb93a267f590853672d3bd1b6
tedu <tedu@DESKTOP-67KFJT0>
2019-05-07 14:20:42 +0800

    1

---
+++ sp09-feign/src/main/java/com/tedu/sp09/Sp08FeignApplication.java
@@ -0,0 +1,18 @@
+package com.tedu.sp09;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+@EnableDiscoveryClient
+@EnableFeignClients
+@SpringBootApplication
+@EnableCircuitBreaker
+public class Sp08FeignApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sp08FeignApplication.class, args);
+	}
+
+}
